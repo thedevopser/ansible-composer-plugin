88a53da8fc3274279a0564fb32a1fc08143bf277
The DevOpser <sylvain.anglade@gmail.com>
2022-05-18 12:23:44 +0200

    feat(plugin): add IO to process

--- src/AnsibleComposerPlugin.php
+++ src/AnsibleComposerPlugin.php
@@ -43,7 +43,7 @@ class AnsibleComposerPlugin implements PluginInterface, EventSubscriberInterface
     {
         $this->io->write('<info>AnsibleComposerPlugin:</info> Installation en cours...');
 
-        $process = new Process(['composer', 'thedevopser:install']);
+        $process = new Process(['composer', 'thedevopser:install'], null, null , $this->io);
         $process->run();
 
         $this->io->write($process->getOutput());
